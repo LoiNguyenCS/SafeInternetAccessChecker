16b3d889b0113c52f57b5baa4322e60e01e691f0
Loi Nguyen <loinguyen@DESKTOP-AI0HVGR>
2025-02-19 17:22:43 -0800

    remove unnecessary codes

--- src/test/kotlin/org/detekt/ucr/ConnectToInternetEffectRuleTest.kt
+++ src/test/kotlin/org/detekt/ucr/ConnectToInternetEffectRuleTest.kt
@@ -2,17 +2,21 @@ import io.gitlab.arturbosch.detekt.api.Config
 import io.gitlab.arturbosch.detekt.rules.KotlinCoreEnvironmentTest
 import io.gitlab.arturbosch.detekt.test.compileAndLintWithContext
 import io.kotest.matchers.collections.shouldHaveSize
-import org.example.detekt.ConnectToInternetEffectRule
+import org.detekt.ucr.rule.ConnectToInternetEffectRule
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.cli.jvm.config.addJvmClasspathRoot
+import org.jetbrains.kotlin.cli.jvm.config.jvmClasspathRoots
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.io.File
 
 @KotlinCoreEnvironmentTest
 internal class InternetConnectEffectRuleTest(private val env: KotlinCoreEnvironment) {
 
     @Test
-    fun `should not detect internet call inside try-catch block`() {
+    fun `should not report internet call inside try-catch block`() {
         val safeCode = """
-            import effects.HasRiskyInternetConnection
+            import org.detekt.ucr.effect.HasRiskyInternetConnection
             class NetworkClient {
                 @HasRiskyInternetConnection
                 fun fetchData() {
@@ -33,13 +37,21 @@ internal class InternetConnectEffectRuleTest(private val env: KotlinCoreEnvironm
     }
 
     @Test
-    fun `should detect internet call outside try-catch block`() {
+    fun `should report internet call outside try-catch block`() {
         val code = """
-        class A {
-          class B
-        }
+            import org.detekt.ucr.effect.HasRiskyInternetConnection
+            class NetworkClient {
+                @HasRiskyInternetConnection
+                fun fetchData() {
+                    println("Fetching data from the internet")
+                }
+
+                fun unsafeMethod() {
+                    fetchData()  // This should be flagged
+                }
+            }
         """
         val findings = ConnectToInternetEffectRule(Config.empty).compileAndLintWithContext(env, code)
-        findings shouldHaveSize 0
+        findings shouldHaveSize 1
     }
 }

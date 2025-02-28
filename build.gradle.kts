f00d36f814e2aff17e37d8bfd5c8b479aa5fa4c0
Loi Nguyen <loinguyen@DESKTOP-AI0HVGR>
2025-02-27 17:55:03 -0800

    add dependency for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,14 @@ version = "1.0-SNAPSHOT"
 dependencies {
     compileOnly("io.gitlab.arturbosch.detekt:detekt-api:1.23.7")
 
+    // All the implementations below are for testing purpose
     testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.23.7")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+    implementation("com.squareup.okhttp3:okhttp")
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
 }
 
 kotlin {
